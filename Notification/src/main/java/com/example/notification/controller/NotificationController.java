e14f7112a0ef213928c47a3fd1f70132a138c089
javad <m.javad.nasrolla@gmail.com>
2022-10-14 20:40:45 +0330

    fix bug for notif module

---
+++ Notification/src/main/java/com/example/notification/controller/NotificationController.java
@@ -0,0 +1,18 @@
+package com.example.notification.controller;
+
+import com.example.notification.model.UserModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/notification")
+public class NotificationController {
+    @GetMapping("/send-welcome/{userModel}")
+    public void getUser(@PathVariable("userModel") UserModel userModel)
+    {
+        String email = userModel.getEmail();
+    }
+}
+
--- Register/src/main/java/com/example/register/service/impl/UserServiceImpl.java
+++ Register/src/main/java/com/example/register/service/impl/UserServiceImpl.java
@@ -6,17 +6,28 @@ import com.example.register.mapper.UserMapper;
 import com.example.register.model.UserModel;
 import com.example.register.service.UserService;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
-public record UserServiceImpl(UserDAO userDAO )  implements UserService {
+public record UserServiceImpl(UserDAO userDAO , RestTemplate restTemplate)  implements UserService {
     @Override
     public void saveUser(UserModel userModel) {
         UserMapper userMapper = new UserMapper();
         User user = userMapper.toEntity(userModel);
 //        user.setPassword(passwordEncoder.encode(userModel.getPassword()));
         userDAO.save(user);
+
+        String url = "http://notification/notification/send-welcome/{userModel}";
+        Map<String, UserModel> urlParams = new HashMap<>();
+        urlParams.put("userModel", userModel);
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(url);
+        restTemplate.getForObject(builder.buildAndExpand(urlParams).toUri(), UserModel.class);
+
 
     }
 
