b60ae80213081058fcfef434394ccf4b81060add
javad <m.javad.nasrolla@gmail.com>
2022-10-14 18:40:47 +0330

    password encode bean

--- Register/src/main/java/com/example/register/RegisterApplication.java
+++ Register/src/main/java/com/example/register/RegisterApplication.java
@@ -2,12 +2,21 @@ package com.example.register;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class RegisterApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RegisterApplication.class, args);
+    }
+
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
